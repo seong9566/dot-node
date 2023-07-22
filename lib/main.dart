dbe09bfec2b94302c1550794105b49a3b02f9596
seong9566 <dlgustjd9566@gmail.com>
2023-07-22 11:54:29 +0900

    ios lock오류 파일 수정

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:dot_node/view/pages/auth/login_page.dart';
 import 'package:dot_node/core/util/locale_string.dart';
-import 'package:dot_node/web_view/pages/auth/web_sign_up_page.dart';
+import 'package:dot_node/web_view/pages/auth/web_login_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
@@ -72,8 +72,8 @@ class MyWeb extends StatelessWidget {
           debugShowCheckedModeBanner: false,
           translations: LocaleString(),
           locale: const Locale('kr_KR'), // kr_KR, en_US
-          home: const WebSignUpPage(),
-          //home: const WebLoginPage(),
+          //home: const WebSignUpPage(),
+          home: const WebLoginPage(),
           //home: const HomePage(),
           //home: PersonalPage(),
         );
